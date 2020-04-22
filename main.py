507ba411e66b1936e98514e0488b739162c95bbf
asamra18 <anandsamra@hotmail.com>
2020-04-21 21:35:00 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ if __name__ == '__main__':
 
     elif question == "1.2":
 
-        with gzip.open(os.path.join('..', 'data', 'mnist.pkl.gz'), 'rb') as f:
+        with gzip.open(os.path.join('mnist.pkl.gz'), 'rb') as f:
             train_set, valid_set, test_set = pickle.load(f, encoding="latin1")
 
 
